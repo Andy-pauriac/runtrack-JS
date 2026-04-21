0f1f73ce00c940ec7c330fe828aceb87894e81de
Andy pauriac <andy.pauriac@laplateforme.io>
2026-04-21 16:15:33 +0200

    job 2 edit

---
+++ jour 02/Job 02/script.js
@@ -0,0 +1,8 @@
+let button = document.getElementById('button')
+button.addEventListener('click', article);
+
+function article() {
+    button = []
+    button[0] = "La vie a plus d'imagination que nous !"
+    console.log(button[0])
+}  
