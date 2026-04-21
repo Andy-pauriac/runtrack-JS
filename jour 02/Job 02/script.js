a1d274043a54af8a146987c358cb2134aca74c3d
Andy pauriac <andy.pauriac@laplateforme.io>
2026-04-21 17:06:19 +0200

    add script.js and modify index.html

--- jour 02/Job 02/script.js
+++ jour 02/Job 02/script.js
@@ -1,8 +1,14 @@
-let button = document.getElementById('button')
-button.addEventListener('click', article);
+function showhide() {
+  let article = document.getElementById("mon-article");
 
-function article() {
-    button = []
-    button[0] = "La vie a plus d'imagination que nous !"
-    console.log(button[0])
-}  
+  if (article) {
+    // L'article existe → on le supprime
+    article.remove();
+  } else {
+    // L'article n'existe pas → on le crée
+    let nouvelArticle = document.createElement("article");
+    nouvelArticle.id = "mon-article";
+    nouvelArticle.textContent = "L'important n'est pas la chute, mais l'atterrissage.";
+    document.body.appendChild(nouvelArticle);
+  }
+}
